08802359cc2fb716fcafd09803f834af66d1d36f
Alexander Bulavkin <Novamarines@mail.ru>
2023-06-24 14:56:27 +0300

    решение задач

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,4 +1,26 @@
-﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-// a = 5; b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
+﻿//  Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
+//  какое число большее, а какое меньшее.
+//  a = 5; b = 7 -> max = 7
+//  a = 2 b = 10 -> max = 10
+//  a = -9 b = -3 -> max = -3
+
+int max = 0;
+int min = 0;
+Console.WriteLine("Введите два не равных числа: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Первое число равно {number1} ");
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Второе число равно {number2} ");
+
+if (number1 > number2)
+{
+    max = number1;
+    min = number2;
+}
+else
+{
+    max = number2;
+    min = number1;
+}
+Console.WriteLine("Большее число равно " + max + " а " + "меньшее число равно " + min);
--- Task04/Program.cs
+++ Task04/Program.cs
@@ -2,3 +2,29 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+
+int max = 0;
+Console.WriteLine("Введите три не равных числа: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Первое число равно {number1} ");
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Второе число равно {number2} ");
+Console.WriteLine("Введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Третье число равно {number3} ");
+if (number1 > max)
+{
+    max = number1;
+}
+if (number2 > max)
+{
+    max = number2;
+
+}
+if (number3 > max)
+{
+    max = number3;
+
+}
+Console.WriteLine("Большее число равно " + max);
